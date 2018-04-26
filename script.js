6899fa4d8ccc9b1f5fd67b7d98139e6954d31fcf
Priscilla Okawa <prisoka@gmail.com>
2018-04-26 09:51:58 -0700

    added solution for random color

---
+++ script.js
@@ -0,0 +1,19 @@
+let body = document.getElementsByTagName("body")[0];
+
+for (let i = 0; i<63; i++){
+  let div = document.createElement("div");
+  div.style.width = "11.1%";
+  div.style.float = "left";
+  div.style.paddingBottom = "11.1%";
+
+  function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  div.style.backgroundColor = getRandomColor()
+  body.appendChild(div);
+}
